a2a7176ecb16addcf8bb3e92a452ae25702896e4
Stuart Leeks <stuartle@microsoft.com>
2025-03-27 13:23:05 +0000

    Pending changes exported from your codespace

---
+++ .stuartle.tmp/env.sh
@@ -0,0 +1,4 @@
+export OPENAI_API_KEY=fake-key
+# export OPENAI_API_KEY=bd27ad7ef76a45ce8ebb4e1a121abfd6
+# export AZURE_POOL_ENDPOINT=fake-endpoint
+export AZURE_POOL_ENDPOINT=https://northeurope.dynamicsessions.io/subscriptions/f95180d7-b5fc-49c8-a9a7-5b3ee0d8ffc6/resourceGroups/acads/sessionPools/dspool1
